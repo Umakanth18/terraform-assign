2fe22583d3712af018c2eda3d23ff749d340fd02
Umakanth18 <Umakanth18@gmail.com>
2025-09-01 18:44:00 +1000

    Added 3 public subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
