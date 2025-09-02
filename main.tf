8504fa5f3e90a93698ff847e6efd3c6fec2bd183
Umakanth18 <Umakanth18@gmail.com>
2025-09-02 20:36:46 +1000

    Added provided information

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 data "aws_availability_zones" "available" {
@@ -7,10 +22,11 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "public" {
-  count             = var.public_subnet_count
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + var.public_subnet_offset)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + var.public_subnet_offset)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
@@ -42,15 +58,35 @@ resource "aws_route_table_association" "public" {
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
-  filter{
+
+  filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
+  }
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web" {
-subnet_id = aws_subnet.public[0].id
-ami = data.aws_ami.amazon_linux.id
-instance_type = "t2.micro"
-associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public[0].id
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web.id]
 }
